a2883167f760effebd8c078c39a60a355451a16a
Leon Kühne <lxk1170@rit.edu>
2022-02-22 05:50:54 -0500

    deletion exists

--- display.py
+++ display.py
@@ -8,15 +8,15 @@ class Display(Tk):
     def __init__(self):
         super().__init__()
 
-        self.selected_node = None
-        self.nodes = []
-        self.line_type = 'action'
         self.line_colors = {
             'action': 'blue',
             'property': 'green',
             'exception': 'red',
             'unknown': 'orange',
         }
+
+        # State
+        self.reset()
 
         # Configure the canvas
         self.canvas = Canvas(self, bg="black")
@@ -26,87 +26,182 @@ class Display(Tk):
         quit_button = Button(self, text="Quit", command=self.destroy)
         quit_button.place(x=0, y=0)
         
+        # Create a way out
+        def clean_nodes():
+            nodes = self.ui_nodes.keys()
+            self.reset()
+            self.canvas.delete('all')
+            self.toggle_line_type()
+            self.load_nodes(nodes)
+        clean_button = Button(self, text="Cleanup", command=clean_nodes)
+        clean_button.place(x=0, y=60)
+
         # Toggle line color
-        color_button = Button(self, text="Line Type", command=self.toggle_line_type)
-        color_button.place(x=0, y=60)
+        self.color_button = Button(self, text="Line Type", command=self.toggle_line_type)
+        self.color_button.place(x=0, y=90)
+        self.toggle_line_type()
 
-        # Add some dynamics
+        # Create/delete nodes
         def on_double_click(e):
-            if e.widget == self.canvas:
-                self.add_node(Node(f"Node{randint(0, 999)}", e.x, e.y))
-
+            if isinstance(e.widget, Canvas):
+                node = Node(f"Node{randint(0, 999)}", e.x, e.y)
+                self.add_node(node)
+                self.select_node(node)
+            elif isinstance(e.widget, Button):
+                node = self.get_node(e.widget)
+                self.delete_node(node)
         self.bind('<Double-Button-1>', on_double_click)
 
+    def reset(self):
+        self.selected_node = None
+        self.selected_button = None
+        self.line_type = 'unknown'
+        self.ui_nodes = {}
+        self.paths = {}
+
     def save_to_file(self, project_path):
-        on_click = lambda: save_nodes(self.nodes, project_path)
+        on_click = lambda: save_nodes(list(self.ui_nodes.keys()), project_path)
         save_button = Button(self, text="Save", command=on_click)
         save_button.place(x=0, y=30)
 
     def toggle_line_type(self):
-        self.line_type = ('action' if self.line_type == 'property' else 'property')
+        types = list(self.line_colors.keys())
+        type_idx = types.index(self.line_type)
+        next_idx = (type_idx + 1) % len(types)
+        self.line_type = types[next_idx]
+        self.color_button.configure(bg=self.line_colors[self.line_type])
 
-    def add_node(self, *nodes):
-        print('adding node', nodes)
+    def delete_node(self, node):
+        # Remove connected lines for children and parents
+        for path_type, children in node.children.items():
+            for child in children:
+                self.remove_path(node, child, path_type)
+        for path_type, parents in node.parents.items():
+            for parent in parents:
+                self.remove_path(parent, node, path_type)
 
-        for node_idx in range(len(nodes)):
-            node = nodes[node_idx]
-            #print(node)
-
-            # add it to the display
-            on_click = lambda: self.select_node(node)
-            node_button = Button(self, text=node.name, command=on_click)
-            node_button.place(x=node.x, y=node.y)
-
-            # Add lines for actions
-            for child in node.actions:
-                self.canvas.create_line(
-                    node.x, node.y, child.x, child.y,
-                    arrow='last', fill="blue", width=5)
-
-            # Add lines for data properties
-            for child in node.properties:
-                self.canvas.create_line(
-                    node.x, node.y, child.x, child.y,
-                    arrow='last', fill="green", width=5)
-
-            # Add line for a new children
-            if self.selected_node:
-                self.connect_nodes(self.selected_node, node)
-
-        self.nodes += nodes
-
-    def connect_nodes(self, parent, child):
-        if self.line_type == 'action':
-            parent.add_action(child)
-        elif self.line_type == 'property':
-            parent.add_property(child)
-        elif self.line_type == 'exception':
-            parent.add_exception(child)
-
-        color = self.line_colors[self.line_type]
-        self.canvas.create_line(
-            parent.x, parent.y,
-            child.x, child.y, 
-            arrow='last', fill=color, width=5)
-
-    def select_node(self, node):
+        # Remove it from the GUI
+        button = self.ui_nodes[node]
+        button.destroy()
+        del self.ui_nodes[node]
         if self.selected_node == node:
             self.selected_node = None
+    
+        print("deleted node")
+
+    def add_node(self, node):
+        # Button for the GUI
+        self.draw_node(node)
+
+        # A line for new children
+        if self.selected_node:
+            self.toggle_path(self.selected_node, node)
+
+        print("added node")
+
+    def load_nodes(self, nodes):
+        # Button for the GUI
+        for node in nodes:
+            self.draw_node(node)
+
+        # Lines for existing children
+        for node in nodes:
+            for child_type, children in node.children.items():
+                color = self.line_colors[child_type]
+                for child in children:
+                    self.draw_path(node, child, child_type)
+
+    def draw_node(self, node):
+        node_button = Button(self, text=node.name)
+        self.ui_nodes[node] = node_button
+        node_button.place(x=node.x, y=node.y)
+        node_button.configure(command=lambda: self.select_node(node))
+
+    def draw_path(self, parent, child, child_type):
+        (c_width, c_height) = self.node_size(child)
+        (p_width, p_height) = self.node_size(parent)
+        print(f"{parent.name} -> {child.name}: {child_type}")
+
+        color = self.line_colors[child_type]
+        (dest_x, dest_y) = self.corner_pos(parent, child)
+        line = self.canvas.create_line(
+            parent.x + p_width/2, parent.y + p_height/2,
+            dest_x + c_width/2, dest_y + c_height/2,
+            arrow='last', fill=color, width=5)
+
+        # keep track of the lines for deleting
+        key = (parent, child, child_type)
+        self.paths[key] = line
+
+    def node_size(self, node):
+        button = self.ui_nodes[node]
+        return (button.winfo_width(), button.winfo_height())
+
+    def corner_pos(self, parent, child):
+        (c_width, c_height) = self.node_size(child)
+        (p_width, p_height) = self.node_size(parent)
+
+        if parent.y - p_height > child.y + c_height:
+            y = child.y + c_height/2
+        elif parent.y + p_height > child.y - c_height:
+            y = child.y 
+        else:
+            y = child.y - c_height/2
+        if parent.x - p_width > child.x + c_width:
+            x = child.x + c_width/2
+        elif parent.x + p_width > child.x - c_width:
+            x = child.x 
+        else:
+            x = child.x - c_width/2
+        return (x, y)
+    
+    def add_path(self, parent, child, line_type):
+        parent.add_child(self.line_type, child)
+        self.draw_path(parent, child, line_type)
+
+    def remove_path(self, parent, child, line_type):
+        key = (parent, child, line_type)
+        parent.remove_child(line_type, child)
+        self.canvas.delete(self.paths[key])
+        del self.paths[key]
+
+    def toggle_path(self, parent, child):
+        if parent.has_child(self.line_type, child):
+            self.remove_path(parent, child, self.line_type)
+        else:
+            self.add_path(parent, child, self.line_type)
+
+    def get_node(self, node_button):
+        for node, button in self.ui_nodes.items():
+            if node_button == button:
+                return node
+
+    def select_node(self, node):
+        button = self.ui_nodes[node]
+
+        if self.selected_node == node:
+            self.selected_node = None
+            button.config(relief='raised')
+            button.configure(bg='white')
             print(f"deselected {node.name}")
         else:
-            # connect the previously selected node to the newly selected one
+            # connect prev selected node to newly selected node
             if self.selected_node:
-                self.connect_nodes(self.selected_node, node)
-                print(f"connected {self.selected_node.name} and {node.name}")
+                self.toggle_path(self.selected_node, node)
+                self.selected_button.config(relief='raised')
+                self.selected_button.configure(bg='white')
 
             self.selected_node = node
+            self.selected_button = button
+            button.config(relief='sunken')
+            button.configure(bg='grey')
             print(f"selected {node.name}")
 
 if __name__ == '__main__':
     project_path = 'project.f'
     nodes = load_nodes(project_path) if exists(project_path) else []
-    
+
     win = Display()
-    win.add_node(*nodes)
+    win.load_nodes(nodes)
     win.save_to_file(project_path)
     win.mainloop()
--- node.py
+++ node.py
@@ -1,23 +1,35 @@
-
 class Node():
     def __init__(self, name, x, y):
         self.name = name
         self.x = x
         self.y = y
-        self.actions = []
-        self.properties = []
-        self.exceptions = []
+        self.children = {
+            'action': [],
+            'property': [],
+            'exception': [],
+            'unknown': [],
+        }
+        self.parents = {
+            'action': [],
+            'property': [],
+            'exception': [],
+            'unknown': [],
+        }
 
-    def add_action(self, *actions):
-        self.actions += actions
+    def has_child(self, child_type, child):
+        return child in self.children[child_type]
 
-    def add_property(self, *props):
-        self.properties += props
+    def remove_child(self, child_type, *children):
+        for child in children:
+            self.children[child_type].remove(child)
+            child.parents[child_type].remove(self)
 
-    def add_exception(self, *exceptions):
-        self.exceptions += exceptions
+    def add_child(self, child_type, *children):
+        self.children[child_type] += children
+        for child in children:
+            child.parents[child_type].append(self)
 
     def __str__(self):
-        actions_str = [str(a) for a in self.actions]
-        props_str = [str(p) for p in self.properties]
-        return f"[{self.name}: actions-{actions_str}, properties-{props_str}"
+        children = f"{[(type, arr) for type, arr in self.children.items()]}"
+        return f"[{self.name}: {children}]"
+
