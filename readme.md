6c03605880d1500baddaa1faf73435da80fa17e0
Leon Kühne <lxk1170@rit.edu>
2022-03-30 12:07:13 -0400

    fixed applying group modules and moved todo list to readme

--- readme.md
+++ readme.md
@@ -1,29 +1,39 @@
 # f
 a project to end all projects
+f u, f me, f every body
+
 #### v0.0
+at some point, you too will hit the "oh shit" moment
 
-at some point, you will too hit the "oh shit" moment; this is how it should have always been done
-when you hit that point, join the cult
+## Todo
+### NODEL
+- feature: indicate groups with double border
+### EDITOR
+- feature: delete node button
+- extra: code auto test field
+- extra: code compile check & indicator
+- extra: pan node view
+- extra: scroll to zoom
+- extra: offscreen node indicator (dot on edge)
+### MODULES
+- fix: group modules to properly save node names
+### PROJECT
+- feature: parse from javascript file
+- feature: export to javascript file
 
-
-### Project Configuration
-Sorted by different levels of abstraction:
->view > ctrl > core
-
-Here's the idea, as an ideal:
-
-#### VIEW
+## Architecture
+Arranged by levels of abstraction:
+> view > ctrl > core
+### VIEW
 - builds 'state'
 - html elements 'graphics' logic
-
-#### CTRL
+### CTRL
 - modifies 'state'
 - high level 'business' logic
-
-#### CORE
+### CORE
 - no 'state'
 - low level 'procedural' logic
 
-#### Why not use MVC?
-I am. If you're stuck on naming things a "standardized" way, then you're probably also stuck on using "features" of MVC. All frameworks are starting points meant for deviation; if you design for one or happen upon one. So is this MVC? Idk, are you racist? Life is not not black and white, just hues in-between. Grow up and stop boxing people in. If you still don't understand, then here; It's grouped similarly to MVC, but in no way am I limiting it to the specifications of a [three tier architecture](https://www.ibm.com/cloud/learn/three-tier-architecture).
+### Why not use MVC?
+I am. "Standards" are meant to be tested. Frameworks expect deviation; framework "features" is an oxymoron. F limiting myself to the [three tier architecture specs](https://www.ibm.com/cloud/learn/three-tier-architecture).
 
--- src/ctrl/node-manager.js
+++ src/ctrl/node-manager.js
@@ -16,48 +16,67 @@ class NodeManager {
   }
 
   createNode(module, x, y) {
-    const nm = this.state.nodel.manager
     if (module.isGroup()) {
-
       // create group node
-      nm.pauseDraw()
-      const groupNodeId = this.createFromMap(module.nodes, x, y)
-      nm.createGroup(groupNodeId, module.name)
-      nm.toggleGroup(groupNodeId)
-      nm.unpauseDraw()
-      return groupNodeId
+      return this.createGroup(module, x, y)
     } else {
-
       // create regular node
-      return nm.addNode(module.base, x, y, module.data())
+      return this.state.nodel.manager.addNode(module.base, x, y, module.data())
     }
+  }
+  createGroup(module, x, y, head=null) {
+    const nm = this.state.nodel.manager
+    nm.pauseDraw()
+    const groupNodeId = this.createFromMap(module.nodes, x, y, head)
+    nm.createGroup(groupNodeId, module.name)
+    nm.toggleGroup(groupNodeId, true)
+    nm.unpauseDraw()
+    return groupNodeId
   }
   updateNode(module, node) {
     if (module.isGroup()) {
+      const nm = this.state.nodel.manager
+      nm.pauseDraw()
       // TODO
-      // do the same as above in create node, but use the node as the head node
+      // delete the previous groups children if collapsed
+      if (node.isGroup(true)) {
+        nm.eachChild(node, child => {
+          nm.deleteNode(child.id)
+        }, node.group.ends)
+        node.children = []
+      }
+
+      // upgrade any node to a group node
+      this.createGroup(module, 0, 0, node)
       console.error('TODO')
     } else {
       node.data = module.data()
     }
   }
-  createFromMap(groupMap, x, y, group=null, connection=null) {
+  createFromMap(groupMap, x, y, head=null, group=null, connection=null) {
     const nm = this.state.nodel.manager
     const moduleManager = this.state.manager.module
-    // use the node name as the key for modules, otherwise fallback to the base template
     const module = moduleManager.get(groupMap.id) || moduleManager.getDefault()
-    const nodeX = x + groupMap.offsetX
-    const nodeY = y + groupMap.offsetY
-
-   
-    // create the node
-    // TODO don't create the node if it already exists (ie. support diamond connections)
-    const nodeId = this.createNode(module, nodeX, nodeY)
+    
+    let nodeId = null
+    let nodeX = null
+    let nodeY = null
+    if (head) {
+      nodeX = head.x
+      nodeY = head.y
+      nodeId = head.id
+    } else {
+      nodeX = x + groupMap.offsetX
+      nodeY = y + groupMap.offsetY
+      // create the node
+      // TODO don't create the node if it already exists (ie. support diamond connections)
+      nodeId = this.createNode(module, nodeX, nodeY)
+    }
 
     // recursively create nodes in group
     for (const [connectionType, children] of Object.entries(groupMap.children)) {
       for (const childGroupMap of children) {
-        this.createFromMap(childGroupMap, nodeX, nodeY, null, {
+        this.createFromMap(childGroupMap, nodeX, nodeY, null, null, {
           type: connectionType,
           parentId: nodeId,
         })
